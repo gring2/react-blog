52d29fb3a10452972eff8e1c67dc4ce4fb6a13e6
gring2 <pjw6454@hotmail.com>
2019-03-03 15:22:24 +0900

    webpack.config.server sass_module_ version

--- blog-frontend/config/paths.js
+++ blog-frontend/config/paths.js
@@ -82,6 +82,10 @@ module.exports = {
   appNodeModules: resolveApp('node_modules'),
   publicUrl: getPublicUrl(resolveApp('package.json')),
   servedPath: getServedPath(resolveApp('package.json')),
+
+  globalStyles: resolveApp('src/styles'),
+  ssrJs: resolveApp('src/ssr.js'),
+  ssrBuild: resolveApp('../blog-backend/src/ssr')
 };
 
 
--- blog-frontend/config/webpack.config.server.js
+++ blog-frontend/config/webpack.config.server.js
@@ -2,11 +2,67 @@ const path = require('path');
 const webpack = require('webpack');
 const paths = require('./paths');
 const getClientEnvironment = require('./env');
+const getCSSModuleLocalIdent = require('react-dev-utils/getCSSModuleLocalIdent');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 // 환경변수를 설정하기 위한 설정
 const publicPath = paths.servedPath;
 const publicUrl = publicPath.slice(0, -1);
 const env = getClientEnvironment(publicUrl);
+// style files regexes
+const cssRegex = /\.css$/;
+const cssModuleRegex = /\.module\.css$/;
+const sassRegex = /\.(scss|sass)$/;
+const sassModuleRegex = /\.module\.(scss|sass)$/;
+const isEnvProduction = true;
+
+// Source maps are resource heavy and can cause out of memory issue for large source files.
+const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false';// common function to get style loaders
+const shouldUseRelativeAssetPaths = publicPath === './';
+
+const getStyleLoaders = (cssOptions, preProcessor) => {
+    const loaders = [
+        isEnvProduction && {
+            loader: MiniCssExtractPlugin.loader,
+            options: Object.assign(
+                {},
+                shouldUseRelativeAssetPaths ? { publicPath: '../../' } : undefined
+            ),
+        },
+        {
+            loader: require.resolve('css-loader'),
+            options: cssOptions,
+        },
+        {
+            // Options for PostCSS as we reference these options twice
+            // Adds vendor prefixing based on your specified browser support in
+            // package.json
+            loader: require.resolve('postcss-loader'),
+            options: {
+                // Necessary for external CSS imports to work
+                // https://github.com/facebook/create-react-app/issues/2677
+                ident: 'postcss',
+                plugins: () => [
+                    require('postcss-flexbugs-fixes'),
+                    require('postcss-preset-env')({
+                        autoprefixer: {
+                            flexbox: 'no-2009',
+                        },
+                        stage: 3,
+                    }),
+                ],
+            },
+        },
+    ].filter(Boolean);
+    if (preProcessor) {
+        loaders.push({
+            loader: require.resolve(preProcessor),
+            options: {
+            },
+        });
+    }
+    return loaders;
+};
 
 module.exports = {
 entry: paths.ssrJs,
@@ -23,57 +79,163 @@ entry: paths.ssrJs,
       {
         // oneOf는 내부의 모든 로더를 시도해보고, 해당되는 것이 없다면
         // 최하단의 file-loader를 실행시킵니다
-        oneOf: [
-          {
-            // 자바스크립트 파일은 바벨을 사용하여 변환합니다
-            test: /\.(js|jsx)$/,
-            include: paths.appSrc,
-            loader: require.resolve('babel-loader'),
-            options: {
-              cacheDirectory: true
-            },
-          },
-          /* css와 scss 파일을 불러올 때는
-          css-loaders/locals를 실행하는 것이 중요합니다. 
-          파일을 따로 만들어내지 않기 때문이죠 */
-          {
-            test: /\.css$/,
-            loader: require.resolve('css-loader/locals'),
-          },
-          /* scss의 경우에는 CSS Module이 제대로 작동하도록
-          production과 동일하게 설정하되 
-          여기서도 css-loader/locals를 적용합니다 */
-          {
-            test: /\.scss$/,
-            use: [
+          oneOf: [
+              // "url" loader works like "file" loader except that it embeds assets
+              // smaller than specified limit in bytes as data URLs to avoid requests.
+              // A missing `test` is equivalent to a match.
               {
-                loader: require.resolve('css-loader/locals'),
-                options: {
-                  importLoaders: 1,
-                  modules: true,
-                  localIdentName: '[name]__[local]___[hash:base64:5]'
-                },
+                  test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+                  loader: require.resolve('url-loader'),
+                  options: {
+                      limit: 10000,
+                      name: 'static/media/[name].[hash:8].[ext]',
+                  },
               },
+              // Process application JS with Babel.
+              // The preset includes JSX, Flow, TypeScript, and some ESnext features.
               {
-                loader: require.resolve('sass-loader'),
-                options: {
-                  includePaths: [paths.globalStyles]
-                }
-              }
-            ]
-          },
-          // 만약에 자바스크립트도, 스타일도 아니라면 파일로 취급합니다.
-          // 여기서 emitFile: false 설정이 중요합니다.
-          {
-            loader: require.resolve('file-loader'),
-            exclude: [/\.js$/, /\.html$/, /\.json$/],
-            options: {
-              name: 'static/media/[name].[hash:8].[ext]',
-              // 경로만 만들고, 실제로 파일을 따로 저장하지는 않습니다.
-              emitFile: false
-            },
-          }
-        ]
+                  test: /\.(js|mjs|jsx|ts|tsx)$/,
+                  include: paths.appSrc,
+                  loader: require.resolve('babel-loader'),
+                  options: {
+                      customize: require.resolve(
+                          'babel-preset-react-app/webpack-overrides'
+                      ),
+
+                      plugins: [
+                          [
+                              require.resolve('babel-plugin-named-asset-import'),
+                              {
+                                  loaderMap: {
+                                      svg: {
+                                          ReactComponent: '@svgr/webpack?-svgo![path]',
+                                      },
+                                  },
+                              },
+                          ],
+                      ],
+                      // This is a feature of `babel-loader` for webpack (not Babel itself).
+                      // It enables caching results in ./node_modules/.cache/babel-loader/
+                      // directory for faster rebuilds.
+                      cacheDirectory: true,
+                      cacheCompression: isEnvProduction,
+                      compact: isEnvProduction,
+                  },
+              },
+              // Process any JS outside of the app with Babel.
+              // Unlike the application JS, we only compile the standard ES features.
+              {
+                  test: /\.(js|mjs)$/,
+                  exclude: /@babel(?:\/|\\{1,2})runtime/,
+                  loader: require.resolve('babel-loader'),
+                  options: {
+                      babelrc: false,
+                      configFile: false,
+                      compact: false,
+                      presets: [
+                          [
+                              require.resolve('babel-preset-react-app/dependencies'),
+                              { helpers: true },
+                          ],
+                      ],
+                      cacheDirectory: true,
+                      cacheCompression: isEnvProduction,
+
+                      // If an error happens in a package, it's possible to be
+                      // because it was compiled. Thus, we don't want the browser
+                      // debugger to show the original code. Instead, the code
+                      // being evaluated would be much more helpful.
+                      sourceMaps: false,
+                  },
+              },
+              // "postcss" loader applies autoprefixer to our CSS.
+              // "css" loader resolves paths in CSS and adds assets as dependencies.
+              // "style" loader turns CSS into JS modules that inject <style> tags.
+              // In production, we use MiniCSSExtractPlugin to extract that CSS
+              // to a file, but in development "style" loader enables hot editing
+              // of CSS.
+              // By default we support CSS Modules with the extension .module.css
+              {
+                  test: cssRegex,
+                  exclude: cssModuleRegex,
+                  use: getStyleLoaders({
+                      importLoaders: 1,
+                      sourceMap: isEnvProduction
+                          ? shouldUseSourceMap
+                          : isEnvDevelopment,
+                  }),
+                  // Don't consider CSS imports dead code even if the
+                  // containing package claims to have no side effects.
+                  // Remove this when webpack adds a warning or an error for this.
+                  // See https://github.com/webpack/webpack/issues/6571
+                  sideEffects: true,
+              },
+              // Adds support for CSS Modules (https://github.com/css-modules/css-modules)
+              // using the extension .module.css
+              {
+                  test: cssModuleRegex,
+                  use: getStyleLoaders({
+                      importLoaders: 1,
+                      modules: true,
+                      getLocalIdent: getCSSModuleLocalIdent,
+                  }),
+              },
+              // Opt-in support for SASS (using .scss or .sass extensions).
+              // By default we support SASS Modules with the
+              // extensions .module.scss or .module.sass
+              {
+                  test: sassRegex,
+                  exclude: sassModuleRegex,
+                  use: getStyleLoaders(
+                      {
+                          importLoaders: 2,
+                          sourceMap: isEnvProduction
+                              ? shouldUseSourceMap
+                              : isEnvDevelopment,
+                      },
+                      'sass-loader'
+                  ),
+                  // Don't consider CSS imports dead code even if the
+                  // containing package claims to have no side effects.
+                  // Remove this when webpack adds a warning or an error for this.
+                  // See https://github.com/webpack/webpack/issues/6571
+                  sideEffects: true,
+              },
+              // Adds support for CSS Modules, but using SASS
+              // using the extension .module.scss or .module.sass
+              {
+                  test: sassModuleRegex,
+                  use: getStyleLoaders(
+                      {
+                          importLoaders: 2,
+                          sourceMap: isEnvProduction
+                              ? shouldUseSourceMap
+                              : isEnvDevelopment,
+                          modules: true,
+                          getLocalIdent: getCSSModuleLocalIdent,
+                      },
+                      'sass-loader'
+                  ),
+              },
+              // "file" loader makes sure those assets get served by WebpackDevServer.
+              // When you `import` an asset, you get its (virtual) filename.
+              // In production, they would get copied to the `build` folder.
+              // This loader doesn't use a "test" so it will catch all modules
+              // that fall through the other loaders.
+              {
+                  loader: require.resolve('file-loader'),
+                  // Exclude `js` files to keep "css" loader working as it injects
+                  // its runtime that would otherwise be processed through "file" loader.
+                  // Also exclude `html` and `json` extensions so they get processed
+                  // by webpacks internal loaders.
+                  exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
+                  options: {
+                      name: 'static/media/[name].[hash:8].[ext]',
+                  },
+              },
+              // ** STOP ** Are you adding a new loader?
+              // Make sure to add the new loader(s) before the "file" loader.
+          ],
       }
     ]
   },
@@ -86,6 +248,14 @@ entry: paths.ssrJs,
   },
   // 여기서는 환경변수 관련 플러그인만 적용해주면 됩니다.
   plugins: [
+      new MiniCssExtractPlugin({
+          // Options similar to the same options in webpackOptions.output
+          // both options are optional
+          filename: 'static/css/[name].[contenthash:8].css',
+          chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
+      }),
     new webpack.DefinePlugin(env.stringified),
+
   ],
 };
+
